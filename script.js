bb00e6e40dc09b4c7bba5a2d29e05fa7f8b1b16f
ibnubatutakamil <ibnubatuta908@gmail.com>
2026-03-27 15:15:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Contoh logika sederhana: Log pesan saat web dimuat
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portofolio Ibnu Batuta Kamil berhasil dimuat!");
+});
+
+// Anda bisa menambahkan fitur interaktif di sini nantinya
